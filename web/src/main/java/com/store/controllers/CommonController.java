f23613cb801463d7631d601203fcafe097dd3451
larinme <maksim_larin@yahoo.com>
2018-08-17 01:06:28 +0300

    spring mvc structure commit root is "/"

---
+++ web/src/main/java/com/store/controllers/CommonController.java
@@ -0,0 +1,15 @@
+package com.store.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/common")
+public class CommonController {
+
+    @RequestMapping("/about")
+    public String about() {
+        System.out.println("about");
+        return "common/about";
+    }
+}
